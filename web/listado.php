81bb4225be4c321766ddce843e491ffd314715c7
abbra <abbra@DESKTOP-PS23F8S>
2018-11-05 14:54:28 -0600

    actualizacio n para detalle recibo

--- web/listado.php
+++ web/listado.php
@@ -1,6 +1,37 @@
 <?php
+$url = 'http://iqrh.viasagt.com';
+   
+$porst['archivo'] = '00100001010000000085_02_Firmado.xml';
+$xml = simplexml_load_file("uploads/xml/".$porst['archivo']); 
+$json = json_encode($xml);
+$xmlArr = json_decode($json, true);
+$RECEPTOR = $xmlArr['Receptor'];
+$correo=$RECEPTOR['CorreoElectronico'];
 
-$url = 'http://iqrh:8080';
+     $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, $url . "/creaPdf.php");
+                curl_setopt($ch, CURLOPT_POST, TRUE);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $porst);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                $remote_server_output = curl_exec($ch);
+                curl_close($ch);
+            //  $correo= $remote_server_output;
+              echo $correo;
+   //          $correo='abrantar@gmail.com';
+   $post['CORREO'] = $correo;
+$ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, $url . "/correo.php");
+                curl_setopt($ch, CURLOPT_POST, TRUE);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                $remote_server_output = curl_exec($ch);
+                curl_close($ch);
+                echo "<br>";
+                echo $remote_server_output;
+                echo "<br>";
+   die();
+   
+   
 $carpetaArchivos = "uploads/xml/";
 $carpeta = $carpetaArchivos . '/';
 $linea = null;
@@ -11,6 +42,7 @@ if (is_dir($carpeta)) {
         while (($archivo = readdir($dir)) !== false) {
             if ($archivo != '.' && $archivo != '..' && $archivo != '.htaccess') {
                 $porst['archivo'] = $archivo;
+                
                 $ch = curl_init();
                 curl_setopt($ch, CURLOPT_URL, $url . "/creaPdf.php");
                 curl_setopt($ch, CURLOPT_POST, TRUE);
@@ -18,12 +50,16 @@ if (is_dir($carpeta)) {
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
                 $remote_server_output = curl_exec($ch);
                 curl_close($ch);
-                $correo = $remote_server_output;
-                if (trim($correo) <> '') {
+                $correo =(string) $remote_server_output;
+                if (trim($correo) == '') {
                     $correo = 'sistemas@eskolor.com';
                 }
-                echo $correo." -- ";
-                $correo = 'abrantar@gmail.com';
+            //    $correo='abrantar@gmail.com';
+                echo $archivo." --> ".$correo." enviado ";
+//                $correo = 'abrantar@gmail.com';
+//               //$correo='yluna@visioneninformatica.com';
+//                $correo='jdepaz@visioneninformatica.com';
+//                echo $correo."<br>";
                 $post['CORREO'] = $correo;
                 $ch = curl_init();
                 curl_setopt($ch, CURLOPT_URL, $url . "/correo.php");
@@ -32,10 +68,24 @@ if (is_dir($carpeta)) {
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
                 $remote_server_output = curl_exec($ch);
                 curl_close($ch);
-                echo $archivo;
                 echo "<br>";
                 echo $remote_server_output;
                 echo "<br>";
+                
+                 $post['CORREO'] = '';
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, $url . "/correo.php");
+                curl_setopt($ch, CURLOPT_POST, TRUE);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                $remote_server_output = curl_exec($ch);
+                curl_close($ch);
+                echo "<br>";
+                echo $remote_server_output;
+                echo "<br>";
+                
+				
+				
 
             }
         }
