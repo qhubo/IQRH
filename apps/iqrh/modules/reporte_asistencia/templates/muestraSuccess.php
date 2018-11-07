0b35c20989d2d3246ee74b93f638fa97b4076637
abbra <abbra@DESKTOP-PS23F8S>
2018-11-07 15:08:39 -0600

    actualiza reporte de empleado

--- apps/iqrh/modules/reporte_asistencia/templates/muestraSuccess.php
+++ apps/iqrh/modules/reporte_asistencia/templates/muestraSuccess.php
@@ -80,7 +80,7 @@
                 <table width="1500px" height="900px" cellpadding="0" cellspacing="0" border="0">
                     <tr style='height:100%;background:white;'>
                         <td>
-                            <iframe id="pes_frame"  src="http://iqrh.viasagt.com/grafica.php?empresa=<?php echo trim($valores['empresa']) ?>"  frameborder="0" style="width:100%; height:100%; overflow-x:hidden;" vspace="0" hspace="0"></iframe>
+                            <iframe id="pes_frame"  src="http://pcr.viasagt.com/grafica.php?empresa=<?php echo trim($valores['empresa']) ?>"  frameborder="0" style="width:100%; height:100%; overflow-x:hidden;" vspace="0" hspace="0"></iframe>
                         </td>
                     </tr>
                 </table>                
