2b819bffe3f255d43e9fb28c5a8129695cceea27
Abraham Araujo <Abraham Araujo@DESKTOP-7I7BVQF.mshome.net>
2019-01-09 15:52:25 -0600

    actualizacion de codigos

--- apps/iqrh/modules/proceso/actions/actions.class.php
+++ apps/iqrh/modules/proceso/actions/actions.class.php
@@ -43,7 +43,7 @@ class procesoActions extends sfActions {
 
         $registros = ReciboEncabezadoQuery::create()
                 ->filterByEnviadoCorreo(false)
-                ->setlimit(1)
+                ->setlimit(5)
                 ->find();
 
         foreach ($registros as $planilla) {
