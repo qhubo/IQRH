0d232ae8e9b4a01d7bc6990aa6a0738d8ad92066
Abraham Araujo <Abraham Araujo@DESKTOP-7I7BVQF.mshome.net>
2019-01-22 08:30:27 -0600

    envio de corre

--- apps/iqrh/modules/proceso/actions/actions.class.php
+++ apps/iqrh/modules/proceso/actions/actions.class.php
@@ -41,18 +41,13 @@ class procesoActions extends sfActions {
         echo $url;
         echo "<br>";
         $parametro = ParametroQuery::create()->findOne();
-
-
         $registros = ReciboEncabezadoQuery::create()
                 ->filterByEnviadoCorreo(false)
                 ->setlimit(4)
                 ->find();
-
         foreach ($registros as $planilla) {
             $id = $planilla->getCabeceraIn();
             $codigo = $planilla->getCodigo();
-
-
             $cabecera = ReciboCabeceraQuery::create()
                     ->filterByCabeceraIn($id)
                     ->findOne();
@@ -65,17 +60,20 @@ class procesoActions extends sfActions {
                     ->find();
 
             if (count($detalle) > 0 && count($cabecera) > 0) {
+                $codigoEmpleado = $encabezado->getCodigo();
+                $usuarioQ = UsuarioQuery::create()->findOneByCodigo($codigoEmpleado);
+                $correcoC = $usuarioQ->getCorreo();
 
                 $html = $this->getPartial('reporte/recibo', array("muestra" => 0,
                     'cabecera' => $cabecera,
                     'encabezado' => $encabezado,
                     'detalle' => $detalle
                 ));
+                echo $correcoC;
+                echo "<br>";
 //                echo $html;
 //                die();
                 $texto = 'Estimad@ ' . $planilla->getEmpleado() . "  adjunto encontrara su recibo de pago.";
-
-
                 $pdf = new sfTCPDF("P", "mm", "Letter");
                 $this->id = $request->getParameter("id");
                 $pdf->SetCreator(PDF_CREATOR);
@@ -100,13 +98,13 @@ class procesoActions extends sfActions {
                 $pdf->SetFont('helvetica', '', 9);
                 $pdf->AddPage();
                 $pdf->writeHTML($html);
-                $ruta = sfConfig::get("sf_upload_dir") . DIRECTORY_SEPARATOR . 'Recibo'.'.pdf';
+                $ruta = sfConfig::get("sf_upload_dir") . DIRECTORY_SEPARATOR . 'Recibo' . '.pdf';
                 $pdf->Output($ruta, 'F');
                 $asunto = "Recibo Planilla " . $cabecera->getInicio() . " " . $cabecera->getFin();
                 $correo = $parametro->getUsuarioCorreo();
                 $clave = $parametro->getClaveCorreo();
-                $correcoC = "yluna@visioneninformatica.com";
-                $correcoC ='abrantar@gmail.com';
+                // $correcoC = "yluna@visioneninformatica.com";
+                //$correcoC ='abrantar@gmail.com';
                 $postData['correo'] = $correo;
                 $postData['clave'] = $clave;
                 $postData['servidor'] = $parametro->getSmtpCorreo();
@@ -115,48 +113,34 @@ class procesoActions extends sfActions {
                 $postData['asunto'] = $asunto;
                 $postData['mensaje'] = $texto;
                 $postData['empresa'] = 'IQRH';
-                $postData['archivo'] = 'Recibo'.'.pdf';
-                echo "<pre>";
-                print_r($postData);
-                echo "</pre>";
-
+                $postData['archivo'] = 'Recibo' . '.pdf';
+//                echo "<pre>";
+//                print_r($postData);
+//                echo "</pre>";
                 $handler = curl_init();
                 curl_setopt($handler, CURLOPT_RETURNTRANSFER, TRUE);
                 curl_setopt($handler, CURLOPT_URL, $url);
                 curl_setopt($handler, CURLOPT_POST, true);
                 curl_setopt($handler, CURLOPT_POSTFIELDS, $postData);
                 $resultado = curl_exec($handler);
-                echo $resultado;
-die();
                 curl_close($handler);
-              //  $planilla->setEnviadoCorreo(true);
-                $planilla->save();
-                   echo "<pre>";
-            print_r($resultado);
-            }
-         
+                $planilla->setEnviadoCorreo(true);
+                $planilla->save();               
+                echo "<pre>";
+                print_r($resultado);
+                echo "</pre>";
+          }
         }
-
         die();
     }
+
 //C:\xampp\htdocs\IQRH\apps\iqrh\modules\proceso\actions\actions.class.php
-    public function executeIndex(sfWebRequest $request) {
-   
-        $codigo ='20';
+    public function executeIndex2(sfWebRequest $request) {
+        $codigo = '20';
         $listado = UsuarioVacacionQuery::periodos($codigo);
-        
-        
         echo "<pre>";
         print_r($listado);
         echo "</pre>";
-        die();
-
-
-
-  
-        //echo $dias=  $interval->format('%a');
-
-
         die();
     }
 
@@ -188,19 +172,18 @@ die();
         echo "cantidad " . $cantida;
         die();
     }
-    
-      public function executeVacacion(sfWebRequest $request) {
-          $valor = $request->getParameter('id');
-          $usuarioQ = UsuarioQuery::create()->findOneById($valor);
-          echo $usuarioQ->getNombreCompleto();
-          echo "<br>";
-          echo $usuarioQ->getFechaAlta();
-          echo "<br>";
-          
-    $vacaciones =UsuarioVacacionQuery::periodos($usuarioQ->getCodigo());
-    echo "<pre>";
-print_R($vacaciones);
-die();
-    
-      }
+
+    public function executeVacacion(sfWebRequest $request) {
+        $valor = $request->getParameter('id');
+        $usuarioQ = UsuarioQuery::create()->findOneById($valor);
+        echo $usuarioQ->getNombreCompleto();
+        echo "<br>";
+        echo $usuarioQ->getFechaAlta();
+        echo "<br>";
+        $vacaciones = UsuarioVacacionQuery::periodos($usuarioQ->getCodigo());
+        echo "<pre>";
+        print_R($vacaciones);
+        die();
+    }
+
 }
