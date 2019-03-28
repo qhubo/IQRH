8296009e45048e0e73280914de462a61477bb1b5
Abraham Araujo <Abraham Araujo@DESKTOP-7I7BVQF.mshome.net>
2019-03-27 18:14:40 -0600

    MONEDA LEMPIRA

--- apps/iqrh/modules/reporte_asistencia/templates/_empleado.php
+++ apps/iqrh/modules/reporte_asistencia/templates/_empleado.php
@@ -8,7 +8,7 @@
         <td  style="xborder: 1px solid black;" xwidth="70px" >&nbsp;<font size="-2"><strong>LLEGADAS<br>&nbsp;TARDE</strong></font>  </td>
         <td  style="xborder: 1px solid black;" xwidth="75px" >&nbsp;<font size="-2"><strong>%<br>PUNTUALIDAD</strong></font>  </td>
         <td  style="xborder: 1px solid black;" xwidth="70px" >&nbsp;<font size="-2"><strong>HORAS<br>&nbsp; MENSUALES</strong></font>  </td>
-        <td  style="xborder: 1px solid black;" xwidth="70px" >&nbsp;<font size="-2"><strong>HORAS<br>&nbsp; REALES</strong></font> </td>      
+<!--        <td  style="xborder: 1px solid black;" xwidth="70px" >&nbsp;<font size="-2"><strong>HORAS<br>&nbsp; REALES</strong></font> </td>      -->
         <td  style="xborder: 1px solid black;" xwidth="50px" >&nbsp;<font size="-2"><strong>% HORAS</strong></font></td>
         <td></td>
     </tr>
@@ -21,13 +21,13 @@
             <td  style=" xborder: 1px solid black;" xwidth="150px"  >&nbsp;<font size="-2"><?php echo $regi->getNombreCompleto(); ?></font>  </td>
             <td  style="xborder: 1px solid black;" xwidth="170px"   >&nbsp;<font size="-2"><?php echo $regi->getPuesto(); ?></font>   </td>
             <td  style="xborder: 1px solid black;" xwidth="70px" align="center" >&nbsp;<font size="-1"><?php echo  $dias= $regi->getAsistencia();   //AsistenciaUsuarioQuery::laborados($inicio, $fin, $regi->getUsuario());  ?></font>&nbsp;&nbsp;&nbsp;  </td>
-            <td  style="xborder: 1px solid black;" xwidth="70px" align="center" >&nbsp;<font size="-1"><?php echo $tardes=0;// AsistenciaUsuarioQuery::tardes($inicio, $fin, $regi->getUsuario());  ?></font>  </td>
+            <td  style="xborder: 1px solid black;" xwidth="70px" align="center" >&nbsp;<font size="-1"><?php echo $tardes = AsistenciaUsuarioQuery::tardes($inicio, $fin, $regi->getUsuario());  ?></font>  </td>
             <?php if ($dias >0) { ?>
             <?php $puntualidad =(($tardes *100) /$dias); ?>
             <?php } ?>
             <td  style="xborder: 1px solid black;" xwidth="75px"  align="center" ><font size="-1">&nbsp; <?php echo  round($puntualidad,0); ?>%  </font></td>
             <td  style="xborder: 1px solid black;" xwidth="70px" align="center">&nbsp;<font size="-1"><?php echo $horamensual; ?> </font>  </td>
-            <td  style="xborder: 1px solid black;" xwidth="70px" align="center">&nbsp;<font size="-1"><?php echo $reales=0; //AsistenciaUsuarioQuery::Reales($inicio, $fin, $regi->getUsuario());  ?></font> </td>      
+<!--            <td  style="xborder: 1px solid black;" xwidth="70px" align="center">&nbsp;<font size="-1"><?php //echo $reales=0; AsistenciaUsuarioQuery::Reales($inicio, $fin, $regi->getUsuario());  ?></font> </td>      -->
                <?php $horas=0 ?>         
    <?php if  ($reales > $horamensual) {  ?>
             <?php $horas=100 ?>
